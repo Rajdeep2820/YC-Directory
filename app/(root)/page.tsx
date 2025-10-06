bed86a2d80d636ba9b35e662596a345573318f6f
Rajdeep2820 <rajdeepsinghpanwar1@gmail.com>
2025-10-06 23:59:34 +0530

    Live data fetching from Sanity with Typegen integration.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import SearchForm from "../../components/SearchForm";
 import StartupCard from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { STARTUP_QUERY } from "@/sanity/lib/queries";
+import { StartupTypeCard } from "@/components/StartupCard";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+
 
 
 export default async function Home({searchParams} : { 
@@ -8,16 +13,19 @@ export default async function Home({searchParams} : {
   searchParams : Promise<{query?:string}>
 }){
   const query = (await searchParams).query;
+  const {data : posts} = await sanityFetch( { query: STARTUP_QUERY})
 
-  const posts = [{
-    _createdAt : new Date(),
-    views : 56,
-    author : { _id : 1 , name : 'Zack'},
-    description : 'this is a description',
-    image : 'https://images.unsplash.com/photo-1731762524352-b5663f83a830?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    category : 'Roboss',
-    title : 'We Robos'
-  }]
+  console.log(JSON.stringify(posts, null, 2));
+
+  // const posts = [{
+  //   _createdAt : new Date(),
+  //   views : 56,
+  //   author : { _id : 1 , name : 'Zack'},
+  //   description : 'this is a description',
+  //   image : 'https://images.unsplash.com/photo-1731762524352-b5663f83a830?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  //   category : 'Roboss',
+  //   title : 'We Robos'
+  // }]
   return (
     <>
     <section className="pink_container">
@@ -29,12 +37,13 @@ export default async function Home({searchParams} : {
 
     <section className="section_container">
       <p className="text-30-semibold">
-        {query ? `Searching for "${query}"`: "All Startupss..."}
+        {query ? `Searching for ${query}` : "All Startups"}
+        
       </p>
 
       <ul className="mt-7 card_grid">
         {posts.length > 0 ? (
-          posts.map((post : StartupCardType)=>(
+          posts.map((post : StartupTypeCard)=>(
             <StartupCard key={post._id} post={post} />
           ))
         ): (
@@ -42,7 +51,7 @@ export default async function Home({searchParams} : {
         )}
       </ul>
     </section>
-
+        <SanityLive/>
     </>
   );
 }
