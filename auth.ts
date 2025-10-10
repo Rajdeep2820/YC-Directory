bcbb9fada8889e740a4b4201adc55609d37a1df9
Rajdeep2820 <rajdeepsinghpanwar1@gmail.com>
2025-10-11 02:29:24 +0530

    Intial Create Form Setup.

--- auth.ts
+++ auth.ts
@@ -7,22 +7,7 @@ import { writeClient } from "./sanity/lib/write-client";
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [Google],
   callbacks : {
-    // async signIn({user : {name,email,image} , profile : {id , login , bio}}){
-    //   const existingUser = await client.withConfig({useCdn : false}).fetch(AUTHOR_GOOGLE_ID_QUERY , {id : id,
-    //   });
-    //   if(!existingUser){
-    //     await writeClient.create({
-    //       _type : 'author',
-    //       id : id,
-    //       name : name,
-    //       username : login,
-    //       gmail : email,
-    //       img : image,
-    //       bio : bio || " ",
-    //     });
-    //   }
-    // return true;
-    // },
+
     async signIn({ user, profile }) {
       const googleId = profile.sub; // ✅ correct Google unique ID
     
---
+++ components/StartupForm.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const StartupForm = () => {
+    return(
+        <>
+        <div>Form</div>
+        </>
+    )
+}
+export default StartupForm;
